c42141b9a08335e02bb0bf0bf91e564e23215f54
MisbahFatma <mfb8290@rit.edu>
2026-02-16 14:35:45 +0400

    Initial commit for MindEase Assignment 1

---
+++ src/test/java/org/example/mindease/MindEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mindease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
