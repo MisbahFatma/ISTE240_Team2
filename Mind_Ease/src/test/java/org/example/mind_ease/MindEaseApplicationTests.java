e6f38624be5273a071f38b33c9d8a963daf34bc3
MisbahFatma <mfb8290@rit.edu>
2026-02-17 19:34:10 +0400

    Added Mind_Ease Assignment

---
+++ Mind_Ease/src/test/java/org/example/mind_ease/MindEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mind_ease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
